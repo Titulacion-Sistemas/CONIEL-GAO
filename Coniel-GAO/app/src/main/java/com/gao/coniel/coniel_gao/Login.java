c3b04b7cf6ebbf0402abd4420db76850a87af21a
Andrea Anabell Loaiza Gonzaga <andreitaloaiza0511@gmail.com>
2014-10-12 08:03:27 -0500

    Segundo Commit
    Avance

--- Coniel-GAO/app/src/main/java/com/gao/coniel/coniel_gao/Login.java
+++ Coniel-GAO/app/src/main/java/com/gao/coniel/coniel_gao/Login.java
@@ -1,17 +1,68 @@
 package com.gao.coniel.coniel_gao;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.R.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Login extends Activity {
+
+    static String name = "andrea";
+    static String pass = "1234";
+    EditText editName;
+    EditText editPass;
+    Button btnInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editName = (EditText) findViewById(R.id.txtusuario);
+        editPass = (EditText) findViewById(R.id.txtclave);
+        btnInicio = (Button) findViewById(R.id.btnInicio);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editName.getText().toString().equals(name) && editPass.getText().toString().equals(pass)){
+                    Intent intent = new Intent(Login.this , menu.class);
+                    startActivity(intent);
+                    finish();
+                   /* Intent intent = new Intent(activity_menu.this, menu.class);
+                        startActivity(intent);
+                        finish();*/
+                 }
+                else{
+    //                    Toast.makeText(getApplicationContext(), "El usuario introducido no es correcto", Toast.LENGTH_LONG).show();
+                       alerta("Nombre de Usuario o Contraseña incorrecta");
+                    }
+                }
+
+        });
+    }
+
+
+    public void alerta(String cadena) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setMessage(cadena);
+        dialog.setCancelable(true).setTitle("Error de Inicio de Sesión");
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog,int id){
+                        dialog.cancel();
+                    }
+                });
+                dialog.create().show();
     }
 
 
@@ -33,4 +84,5 @@ public class Login extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
